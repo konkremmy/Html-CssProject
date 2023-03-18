5bb717082f192aa66627a5144bdfeb20b746a047
konkremmy <kremmyyo@gmail.com>
2023-03-18 14:02:15 +0200

    Add project files.

---
+++ JS/DeleteButton.js
@@ -0,0 +1,13 @@
+﻿function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+<div class="container">
+    <h1>Are u sure u want to Delete it?</h1>
+
+
+    <div class="clearfix">
+        <button type="button" class="deletebtn">Delete</button>
+        <button type="button" class="cancelbtn">Cancel</button>
+    </div>
+</div>
